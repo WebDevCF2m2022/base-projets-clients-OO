b1f0ee366ea47e8d9de658e5f4221d86b79894cd
mikhawa <michaeljpitz@gmail.com>
2023-06-08 12:12:33 +0200

    TestManager

--- models/Managers/TestManager.php
+++ models/Managers/TestManager.php
@@ -2,7 +2,44 @@
 
 namespace model\Managers;
 
-class TestManager
+use model\Interfaces\ManagersInterfaces;
+use PDO;
+use Exception;
+
+class TestManager implements ManagersInterfaces
 {
 
+    private $connect;
+
+    public function __construct(PDO $connection)
+    {
+        $this->connect = $connection;
+    }
+
+    public function getOneById(int $id)
+    {
+        $this->connect->prepare("SELECT * FROM test WHERE `idTest` = :id");
+        $this->connect->bindValue(":id", $id, PDO::PARAM_INT);
+        try {
+            $this->connect->execute();
+            $result = $this->connect->fetch();
+            return $result;
+        } catch (Exception $e) {
+            echo "Erreur de requête : " . $e->getMessage();
+            exit;
+        }
+    }
+
+    public function getAll()
+    {
+        $this->connect->prepare("SELECT * FROM test");
+        try {
+            $this->connect->execute();
+            $result = $this->connect->fetchAll();
+            return $result;
+        } catch (Exception $e) {
+            echo "Erreur de requête : " . $e->getMessage();
+            exit;
+        }
+    }
 }
